7de41133c717e308610c105095436e28772913c9
charles delannoy <charles.delannoy@hei.fr>
2021-04-28 11:07:37 +0200

    tests for interger_binary_gap

--- spec/exercice_spec.rb
+++ spec/exercice_spec.rb
@@ -39,12 +39,12 @@ describe "Exercice" do
         expect(to_binary(22)).to eq("10110")
       end
 
-      it 'should convert 2147473647 to binary' do
-        expect(to_binary(2147473647)).to eq("1111111111111111101100011101111")
+      it 'should convert 2_147_473_647 to binary' do
+        expect(to_binary(2_147_473_647)).to eq("1111111111111111101100011101111")
       end
 
-      it 'should convert 2147483647 to binary' do
-        expect(to_binary(2147483647)).to eq("1111111111111111111111111111111")
+      it 'should convert 2_147_483_647 to binary' do
+        expect(to_binary(2_147_483_647)).to eq("1111111111111111111111111111111")
       end
     end
   end
@@ -92,6 +92,51 @@ describe "Exercice" do
 
       it 'should return the binary gap for 1111111111111111111111111111111 binary' do
         expect(binary_gap("1111111111111111111111111111111")).to eq(0)
+      end
+    end
+  end
+
+  context '#integer_binary_gap' do
+    describe 'format' do
+      it 'should return an integer' do
+        expect(integer_binary_gap(0)).to be_a(Integer)
+      end
+    end
+    describe 'tests' do
+      it 'should return the binary gap for 0' do
+        expect(integer_binary_gap(0)).to eq(0)
+      end
+
+      it 'should return the binary gap for 1' do
+        expect(integer_binary_gap(1)).to eq(0)
+      end
+
+      it 'should return the binary gap for 2' do
+        expect(integer_binary_gap(2)).to eq(0)
+      end
+
+      it 'should return the binary gap for 5' do
+        expect(integer_binary_gap(5)).to eq(1)
+      end
+
+      it 'should return the binary gap for 6' do
+        expect(integer_binary_gap(6)).to eq(0)
+      end
+
+      it 'should return the binary gap for 8' do
+        expect(integer_binary_gap(8)).to eq(0)
+      end
+
+      it 'should return the binary gap for 22' do
+        expect(integer_binary_gap(22)).to eq(1)
+      end
+
+      it 'should return the binary gap for 2_147_473_647' do
+        expect(integer_binary_gap(2_147_473_647)).to eq(3)
+      end
+
+      it 'should return the binary gap for 2_147_483_647' do
+        expect(integer_binary_gap(2_147_483_647)).to eq(0)
       end
     end
   end
