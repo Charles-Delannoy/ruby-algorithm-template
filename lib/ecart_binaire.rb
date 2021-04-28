178db6e2e451f6b9e998c22c48282dc45b029028
charles delannoy <charles.delannoy@hei.fr>
2021-04-28 11:14:03 +0200

    alternativ version for to_binary

--- lib/ecart_binaire.rb
+++ lib/ecart_binaire.rb
@@ -6,12 +6,15 @@ def to_binary(int)
   return '0' if int.zero?
 
   division = [int, nil]
-  binary_conversion = ''
+  # binary_conversion = ''
+  binary_conversion = []
   until division.first.zero?
     division = division.first.divmod(2)
-    binary_conversion = division.last.to_s + binary_conversion
+    # binary_conversion = division.last.to_s + binary_conversion
+    binary_conversion.insert(0, division.last)
   end
-  binary_conversion
+  # binary_conversion
+  binary_conversion.join
 end
 
 def binary_gap(binary)
