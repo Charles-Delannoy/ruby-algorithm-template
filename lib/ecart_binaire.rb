6007f3f547ab46dfd0d9bea796f23a1e10e36c1a
charles delannoy <charles.delannoy@hei.fr>
2021-04-28 10:39:33 +0200

    binary convertor

--- lib/ecart_binaire.rb
+++ lib/ecart_binaire.rb
@@ -1,3 +1,11 @@
-def exercice_method
-  false
+def to_binary(int)
+  return '0' if int.zero?
+
+  division = [int, nil]
+  binary_conversion = ''
+  until division.first.zero?
+    division = division.first.divmod(2)
+    binary_conversion = division.last.to_s + binary_conversion
+  end
+  binary_conversion
 end
