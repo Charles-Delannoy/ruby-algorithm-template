cc57532b74dd5465c57cd3b9a04eaf5e80c6b5f4
charles delannoy <charles.delannoy@hei.fr>
2021-04-28 10:32:03 +0200

    test for binary convertor

--- spec/exercice_spec.rb
+++ spec/exercice_spec.rb
@@ -2,16 +2,37 @@ require 'ecart_binaire'
 
 describe "Exercice" do
 
-  let(:true_assertion) do
-    true
-  end
+  context '#to_binary' do
+    it 'should convert 0 to binary' do
+      expect(to_binary(0)).to eq("0")
+    end
 
-  it "should assert the truth" do
-    expect(true_assertion).to be(true)
-  end
+    it 'should convert 1 to binary' do
+      expect(to_binary(1)).to eq("1")
+    end
 
-  context '#exercice'
-  it 'should return false' do
-    expect(exercice_method).to be(false)
+    it 'should convert 2 to binary' do
+      expect(to_binary(2)).to eq("10")
+    end
+
+    it 'should convert 5 to binary' do
+      expect(to_binary(5)).to eq("101")
+    end
+
+    it 'should convert 6 to binary' do
+      expect(to_binary(6)).to eq("110")
+    end
+
+    it 'should convert 8 to binary' do
+      expect(to_binary(8)).to eq("1000")
+    end
+
+    it 'should convert 22 to binary' do
+      expect(to_binary(22)).to eq("10110")
+    end
+
+    it 'should convert 2147483647 to binary' do
+      expect(to_binary(2147483647)).to eq("1111111111111111111111111111111")
+    end
   end
 end
